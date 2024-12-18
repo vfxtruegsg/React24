6f4ffcc9d8f6841b6d53ddceb1f427634ea13923
Andrij P. <andrejpopkov565@gmail.com>
2024-12-18 12:35:09 +0300

    formik

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -527,28 +527,76 @@
 // export default CheckboxChecked;
 
 import { useId, useState, useEffect } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Must be a valid email!").required("Required"),
+  message: Yup.string()
+    .min(3, "Too short")
+    .max(256, "Too long")
+    .required("Required"),
+  level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required"),
+});
+
+const initialValues = {
+  username: "",
+  email: "",
+  message: "",
+  level: "good",
+};
 
 const FeedbackForm = () => {
-  const initialValues = {
-    username: "",
-    email: "",
-    comments: "",
-  };
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const msgFieldId = useId();
+  const levelFieldId = useId();
 
-  const submitForm = (values, actions) => {
+  const handleSubmit = (values, actions) => {
     console.log(values);
-
     actions.resetForm();
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={submitForm}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form>
-        <Field type="text" name="username" />
-        <Field type="email" name="email" />
-        <Field as="textarea" name="comments" />
-        <button type="submit">Submit Form</button>
+        <div>
+          <label htmlFor={nameFieldId}>Username</label>
+          <Field type="text" name="username" id={nameFieldId} />
+          <ErrorMessage name="username" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={emailFieldId}>Email</label>
+          <Field type="email" name="email" id={emailFieldId} />
+          <ErrorMessage name="email" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={msgFieldId}>Message</label>
+          <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+          <ErrorMessage name="message" component="span" />
+        </div>
+
+        <div>
+          <label htmlFor={levelFieldId}>Service satisfaction level</label>
+          <Field as="select" name="level" id={levelFieldId}>
+            <option value="good">Good</option>
+            <option value="neutral">Neutral</option>
+            <option value="bad">Bad</option>
+          </Field>
+          <ErrorMessage name="level" component="span" />
+        </div>
+
+        <button type="submit">Submit</button>
       </Form>
     </Formik>
   );
